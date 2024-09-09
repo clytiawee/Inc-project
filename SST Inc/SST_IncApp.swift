b8a9f7486a6263b7ab4d2f700b6a4ea938a04163
Clytia Wee <Clytia_wee_ci_en@s2024.ssts.edu.sg>
2024-09-09 16:10:40 +0800

    Initial Commit

---
+++ SST Inc/SST_IncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SST_IncApp.swift
+//  SST Inc
+//
+//  Created by Clytia Wee Ci En on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SST_IncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
