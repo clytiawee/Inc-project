cea8d06bb7295481eca0b09c0db1374e2a7001a8
random <chuah_cheng_hang@s2024.ssts.edu.sg>
2024-09-23 20:09:16 +0800

    Initial Commit

---
+++ Incoin2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Incoin2
+//
+//  Created by Chuah Cheng Hang on 23/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Incoin2/Incoin2App.swift
@@ -0,0 +1,17 @@
+//
+//  Incoin2App.swift
+//  Incoin2
+//
+//  Created by Chuah Cheng Hang on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Incoin2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
