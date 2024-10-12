8c48e71f99abd25b5dc8d438914ebb147a2473e3
NoUser <chuah_cheng_hang@s2024.ssts.edu.sg>
2024-10-12 16:54:02 +0800

    added storyline to be first page if not seen before. Changed some @State variables to @AppStorage. StoryLine page added need to use darkMode before available to go to contentView

---
+++ SST Inc/Story.swift
@@ -0,0 +1,18 @@
+//
+//  Story.swift
+//  SST Inc
+//
+//  Created by Chuah Cheng Hang on 12/10/24.
+//
+
+import SwiftUI
+
+struct Story: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Story()
+}
